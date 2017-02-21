8e5dbcbdf349ab75bc26d10ddd5aad80d5ff2ee1
William Xu <xuwei@mxchip.com>
2017-02-21 17:57:12 +0800

    Test MiCO API

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
+#include "mico.h"
 
-DigitalOut led1(MICO_SYS_LED);
+DigitalOut led1(MBED_SYS_LED);
 Serial pc(STDIO_UART_TX, STDIO_UART_RX, 115200);
 
 // main() runs in its own thread in the OS
@@ -8,9 +9,11 @@ Serial pc(STDIO_UART_TX, STDIO_UART_RX, 115200);
 int main() {
 	int i = 1;
 	pc.printf("Hello World !\r\n");
+	mico_gpio_initialize( MICO_RF_LED, OUTPUT_PUSH_PULL );
 
     while (true) {
         led1 = !led1;
+        mico_gpio_output_trigger( MICO_RF_LED );
         pc.printf("This program runs since %d seconds.\r\n", i++);
         wait(1);
     }
