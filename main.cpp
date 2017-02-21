e1da9be70619160c61903c45501573a9a0069fe0
William Xu <xuwei@mxchip.com>
2017-02-21 11:23:58 +0800

    Add serial output demo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include "mbed.h"
 
-DigitalOut led1(LED1);
+DigitalOut led1(MICO_SYS_LED);
+Serial pc(STDIO_UART_TX, STDIO_UART_RX, 115200);
 
 // main() runs in its own thread in the OS
 // (note the calls to wait below for delays)
 int main() {
+	int i = 1;
+	pc.printf("Hello World !\r\n");
+
     while (true) {
         led1 = !led1;
-        wait(0.5);
+        pc.printf("This program runs since %d seconds.\r\n", i++);
+        wait(1);
     }
 }
 
