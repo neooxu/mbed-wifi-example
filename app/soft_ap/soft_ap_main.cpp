f28f9cfdc049c4734c2f3a8ddaf7c822cf26c70c
William Xu <xuwei@mxchip.com>
2017-04-01 19:45:52 +0800

    Softap demo optimized

--- app/soft_ap/soft_ap_main.cpp
+++ app/soft_ap/soft_ap_main.cpp
@@ -5,18 +5,15 @@
 #include "EMW10xxInterface.h"
 
 
-Serial soft_ap_pc(STDIO_UART_TX, STDIO_UART_RX, 115200);
-
-
-EMW10xxInterface soft_ap_wifi;
+EMW10xxInterface wlan;
 
 /* MiCO GPIO IO driver demo */
 int app_soft_ap( )
 {
     printf( "start soft ap!\r\n" );
 
-    soft_ap_wifi.set_interface( Soft_AP );
-    int ret = soft_ap_wifi.connect( "mbed_softAP", "12345678", NSAPI_SECURITY_WPA_WPA2, 0 );
+    wlan.set_interface( Soft_AP );
+    int ret = wlan.connect( "mbed_softAP", "12345678", NSAPI_SECURITY_WPA_WPA2, 0 );
 
     if ( ret != NSAPI_ERROR_OK ) {
         printf("Soft ap creation failed\r\n");
