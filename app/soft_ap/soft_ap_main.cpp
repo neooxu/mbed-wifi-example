1f8ad442008e73ed0cdcc1db83ddceeb297fdb5d
William Xu <xuwei@mxchip.com>
2017-04-01 19:43:01 +0800

    Fix some soft ap app errors

--- app/soft_ap/soft_ap_main.cpp
+++ app/soft_ap/soft_ap_main.cpp
@@ -8,24 +8,22 @@
 Serial soft_ap_pc(STDIO_UART_TX, STDIO_UART_RX, 115200);
 
 
-#define os_soft_ap_log(format, ...)  custom_log("softAP", format, ##__VA_ARGS__)
-
 EMW10xxInterface soft_ap_wifi;
 
 /* MiCO GPIO IO driver demo */
 int app_soft_ap( )
 {
-	os_soft_ap_log( "start!\r\n" );
+    printf( "start soft ap!\r\n" );
 
-	int ret = soft_ap_wifi.connect( "mbed_softAP", "12345678", NSAPI_SECURITY_WPA_WPA2, 0, Soft_AP);
-	if (ret != 0) {
-		os_soft_ap_log("\r\nConnection error\r\n");
-		return -1;
-	}
+    soft_ap_wifi.set_interface( Soft_AP );
+    int ret = soft_ap_wifi.connect( "mbed_softAP", "12345678", NSAPI_SECURITY_WPA_WPA2, 0 );
 
-	soft_ap_wifi.disconnect();
+    if ( ret != NSAPI_ERROR_OK ) {
+        printf("Soft ap creation failed\r\n");
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
 
 
